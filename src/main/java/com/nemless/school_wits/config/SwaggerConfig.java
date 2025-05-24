ecaf0d8955ce9d45a19a2b0215e1c4fcbc2a88bd
Fayeaz Ahmed <r10.ahmed@yahoo.com>
2025-05-24 23:00:25 +0600

    properties update

--- src/main/java/com/nemless/school_wits/config/SwaggerConfig.java
+++ src/main/java/com/nemless/school_wits/config/SwaggerConfig.java
@@ -5,15 +5,26 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     private static final String SECURITY_SCHEME_NAME = "BearerAuth";
 
+    @Value("${api.server.url}")
+    private String apiServerUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
+        Server httpsServer = new Server()
+                .url(apiServerUrl)
+                .description("Secure Backend Server");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Your API")
@@ -26,6 +37,7 @@ public class SwaggerConfig {
                                         .name(SECURITY_SCHEME_NAME)
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                                        .bearerFormat("JWT")))
+                .servers(List.of(httpsServer));
     }
 }
